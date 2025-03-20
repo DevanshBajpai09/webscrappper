d16516232b6086f17865a7ab1c54deac3ec5ad28
Devansh bajpai <163446624+DevanshBajpai09@users.noreply.github.com>
2025-03-20 07:39:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins
 
 
 
+
 # Function to scrape data from a URL
 def scrape_website(url):
     try:
@@ -58,4 +59,4 @@ def scrape():
         "summary": summary
     })
 if __name__ == "__main__":
-    app.run(debug=True , port=5000)
+    app.run(debug=True, host="0.0.0.0", port=10000)
